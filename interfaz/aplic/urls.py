fd268b89d4afa678e678a3d016f9f6986f98a8e1
unknown <marcvergara2023@udec.cl>
2025-07-10 20:53:55 -0400

    templates

--- interfaz/aplic/urls.py
+++ interfaz/aplic/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from .views import citas_view, calendario_view
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path('citas/', citas_view, name='citas'),
+    path('calendario/', calendario_view, name='calendario')
 ]
--- interfaz/aplic/views.py
+++ interfaz/aplic/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("Hello World")
+
+def citas_view(request):
+    return render(request, 'citas.html')
+
+def calendario_view(request):
+    return render(request, 'calendario.html')
